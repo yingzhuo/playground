1a3f031f404e6e844056a779671271058ac23468
yingzhuo <yingzhor@gmail.com>
2020-11-26 10:54:49 +0800

    2020-11-26 10:54:49

--- src/main/java/com/github/yingzhuo/playground/ApplicationBootJackson.scala
+++ src/main/java/com/github/yingzhuo/playground/ApplicationBootJackson.scala
@@ -1,21 +1,14 @@
 package com.github.yingzhuo.playground
 
-import java.util.Optional
-
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 
-import scala.jdk.OptionConverters._
-
 @Configuration
 protected class ApplicationBootJackson {
 
   @Autowired(required = false)
-  def config(om: Optional[ObjectMapper]): Unit = om.toScala match {
-    case Some(x) => x.registerModule(DefaultScalaModule)
-    case None => // nop
-  }
+  def config(om: ObjectMapper): Unit = Option(om).foreach(_.registerModule(DefaultScalaModule))
 
 }
