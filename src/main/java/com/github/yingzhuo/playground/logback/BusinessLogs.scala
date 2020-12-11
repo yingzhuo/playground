98442e4b2139b67f29dcc4972ad205e50e9a7559
yingzhuo <yingzhor@gmail.com>
2020-12-11 16:25:53 +0800

    2020-12-11 16:25:53

---
+++ src/main/java/com/github/yingzhuo/playground/logback/BusinessLogs.scala
@@ -0,0 +1,10 @@
+package com.github.yingzhuo.playground.logback
+
+import org.slf4j.{Logger, LoggerFactory}
+
+object BusinessLogs {
+
+  final val BUSINESS_1: Logger = LoggerFactory.getLogger("BUSINESS_1")
+  final val BUSINESS_2: Logger = LoggerFactory.getLogger("BUSINESS_2")
+
+}
--- src/main/java/com/github/yingzhuo/playground/task/ScheduledTasks.scala
+++ src/main/java/com/github/yingzhuo/playground/task/ScheduledTasks.scala
@@ -1,19 +1,24 @@
 package com.github.yingzhuo.playground.task
 
-import java.util.UUID
-
-import org.slf4j.LoggerFactory
+import com.github.yingzhuo.playground.logback.BusinessLogs
+import org.apache.commons.lang3.time.{DateFormatUtils => DateFormat}
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+
+import scala.util.Random
 
 @Component
 protected class ScheduledTasks {
 
-  private val log = LoggerFactory.getLogger(getClass)
-
-  @Scheduled(fixedRate = 3000)
+  @Scheduled(fixedRate = 800)
   def doExecute(): Unit = {
-    log.debug(UUID.randomUUID().toString)
+
+    val date = DateFormat.format(System.currentTimeMillis(), "yyyy-MM-mm HH:mm:ss.SSS")
+    val user = s"user-${Random.between(0, 16)}"
+    val upstream = Random.between(0L, 8192L);
+    val downstream = Random.between(0L, 8192L);
+
+    BusinessLogs.BUSINESS_1.debug(s"$date,$user,$upstream,$downstream")
   }
 
 }
