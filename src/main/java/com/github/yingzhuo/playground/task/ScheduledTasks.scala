dc0522b9694cf721d08cadf99923f71ce86a39b8
yingzhuo <yingzhor@gmail.com>
2020-12-15 17:00:12 +0800

    2020-12-15 17:00:12

--- src/main/java/com/github/yingzhuo/playground/task/ScheduledTasks.scala
+++ src/main/java/com/github/yingzhuo/playground/task/ScheduledTasks.scala
@@ -1,5 +1,7 @@
 package com.github.yingzhuo.playground.task
 
+import java.util.UUID
+
 import com.github.yingzhuo.playground.logback.BusinessLogs
 import org.apache.commons.lang3.time.{DateFormatUtils => DateFormat}
 import org.springframework.scheduling.annotation.Scheduled
@@ -11,7 +13,7 @@ import scala.util.Random
 protected class ScheduledTasks {
 
   @Scheduled(fixedRate = 800)
-  def doExecute(): Unit = {
+  def doExecute1(): Unit = {
 
     val date = DateFormat.format(System.currentTimeMillis(), "yyyy-MM-mm HH:mm:ss.SSS")
     val user = s"user-${Random.between(0, 16)}"
@@ -19,6 +21,11 @@ protected class ScheduledTasks {
     val downstream = Random.between(0L, 8192L);
 
     BusinessLogs.BUSINESS_1.debug(s"$date,$user,$upstream,$downstream")
+  }
+
+  @Scheduled(fixedRate = 1000)
+  def doExecute2(): Unit = {
+    BusinessLogs.BUSINESS_2.debug(UUID.randomUUID().toString)
   }
 
 }
