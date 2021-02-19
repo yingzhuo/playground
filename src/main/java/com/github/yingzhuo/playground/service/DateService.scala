c6904a013e4bdfd90dbd907247d645270d65ae5c
应卓 <yingzhor@gmail.com>
2021-02-19 14:01:59 +0800

    2021-02-19 14:01:59

--- src/main/java/com/github/yingzhuo/playground/service/DateService.scala
+++ src/main/java/com/github/yingzhuo/playground/service/DateService.scala
@@ -1,7 +1,15 @@
 package com.github.yingzhuo.playground.service
 
-trait DateService {
+import org.apache.commons.lang3.time.{DateFormatUtils => DateFormat}
+import org.springframework.stereotype.Service
+
+sealed trait DateService {
 
   def now(pattern: String): String
+
+  @Service
+  private class Default extends AnyRef with DateService {
+    override def now(pattern: String): String = DateFormat.format(System.currentTimeMillis(), pattern)
+  }
 
 }
