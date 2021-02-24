74f5552fcf40c2f8a7afe1c84099793f845c18f2
应卓 <yingzhor@gmail.com>
2021-02-24 13:40:44 +0800

    2021-02-24 13:40:44

--- src/main/java/com/github/yingzhuo/playground/ApplicationBootProperties.scala
+++ src/main/java/com/github/yingzhuo/playground/ApplicationBootProperties.scala
@@ -3,14 +3,16 @@ package com.github.yingzhuo.playground
 import com.github.yingzhuo.springboot.env.propertysource.HoconPropertySourceFactory
 import org.springframework.context.annotation.{Configuration, Profile, PropertySource}
 
-private[playground] class ApplicationBootProperties {
-}
-
 private[playgroud] object ApplicationBootProperties {
 
   @Configuration
-  @PropertySource(value = Array("classpath*:/config/property.conf"), factory = classOf[HoconPropertySourceFactory])
+  @PropertySource(value = Array("classpath*:/config/property-dev.conf"), factory = classOf[HoconPropertySourceFactory])
   @Profile(Array("dev"))
   class Dev
+
+  @Configuration
+  @PropertySource(value = Array("classpath*:/config/property-k8s.conf"), factory = classOf[HoconPropertySourceFactory])
+  @Profile(Array("k8s"))
+  class K8s
 
 }
