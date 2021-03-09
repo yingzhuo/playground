65361e5454ca2d30f528e676a450b7c37c549168
应卓 <yingzhor@gmail.com>
2021-03-09 09:44:31 +0800

    2021-03-09 09:44:31

--- playground-domain/src/main/java/com/github/yingzhuo/playground/domain/Author.scala
+++ playground-domain/src/main/java/com/github/yingzhuo/playground/domain/Author.scala
@@ -3,7 +3,5 @@ package com.github.yingzhuo.playground.domain
 case class Author(name: String, email: Array[String]) extends Serializable
 
 object Author {
-
   final val Ying = Author("应卓", Array("yingzhor@gmail.com"))
-
 }
